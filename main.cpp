e6c5950c56c7eca9e551877d962dec60f52935c6
Franz Beaune <franz@appleseedhq.net>
2019-02-08 11:58:21 +0100

    Create swap chain image views

--- main.cpp
+++ main.cpp
@@ -90,6 +90,7 @@ class HelloTriangleApplication
         create_vk_logical_device();
         create_vk_swap_chain();
         retrieve_vk_swap_chain_images();
+        create_vk_swap_chain_image_views();
     }
 
     static void query_vk_instance_extensions()
@@ -598,6 +599,34 @@ class HelloTriangleApplication
         vkGetSwapchainImagesKHR(m_device, m_swap_chain, &image_count, m_swap_chain_images.data());
     }
 
+    void create_vk_swap_chain_image_views()
+    {
+        std::cout << "Creating Vulkan swap chain image views..." << std::endl;
+
+        m_swap_chain_image_views.resize(m_swap_chain_images.size());
+
+        for (std::size_t i = 0; i < m_swap_chain_images.size(); ++i)
+        {
+            VkImageViewCreateInfo create_info = {};
+            create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            create_info.image = m_swap_chain_images[i];
+            create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            create_info.format = m_swap_chain_surface_format;
+            create_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            create_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            create_info.subresourceRange.baseMipLevel = 0;
+            create_info.subresourceRange.levelCount = 1;
+            create_info.subresourceRange.baseArrayLayer = 0;
+            create_info.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(m_device, &create_info, nullptr, &m_swap_chain_image_views[i]) != VK_SUCCESS)
+                throw std::runtime_error("Failed to create Vulkan image views");
+        }
+    }
+
     void create_window()
     {
         std::cout << "Creating window..." << std::endl;
@@ -620,6 +649,9 @@ class HelloTriangleApplication
     void cleanup()
     {
         std::cout << "Cleaning up..." << std::endl;
+
+        for (const VkImageView image_view : m_swap_chain_image_views)
+            vkDestroyImageView(m_device, image_view, nullptr);
 
         vkDestroySwapchainKHR(m_device, m_swap_chain, nullptr);
         vkDestroyDevice(m_device, nullptr);
