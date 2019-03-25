58a1dedba2325f4562fad9aa8072d7959bb9b012
Franz Beaune <franz@appleseedhq.net>
2019-03-25 15:23:25 +0100

    Set GLM preprocessor definitions in VS project

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include "vku.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+// GLM preprocessor definitions set in project.
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
+
+#include "vku.h"
 
 #include <algorithm>
 #include <array>
@@ -28,16 +36,6 @@
 #include <thread>
 #include <unordered_map>
 #include <vector>
-
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_ENABLE_EXPERIMENTAL
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/hash.hpp>
 
 #define FORCE_VSYNC
 
